e14bc52b28cb1a4ab4a7f2a2a9ae23b0b49fccec
manasvinir <manasvini.raghunathan@gmail.com>
2025-01-22 21:10:27 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.manasvinir.cooksys.basiclemonadestand;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
