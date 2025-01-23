48d4ccbdb221fb436debe48a317eb56e533fa741
manasvinir <manasvini.raghunathan@gmail.com>
2025-01-22 20:06:40 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+package com.github.manasvinir.cooksys.basiclemonadestand;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
